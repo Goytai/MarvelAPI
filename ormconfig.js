513ccb57833a911841e510cf8a44930aa5e5d759
Goytai <dev.goytai@gmail.com>
2021-06-08 13:44:44 -0300

    Fix: database envs

--- ormconfig.js
+++ ormconfig.js
@@ -16,7 +16,7 @@ module.exports = [
   {
     "name": "default",
     "type": process.env.DATABASE_TYPE || "postgres",
-    "host": process.env.DATABASE_URL || "localhost",
+    "host": process.env.DATABASE_HOST || "localhost",
     "port": process.env.DATABASE_PORT || 5432,
     "username": process.env.DATABASE_USERNAME || "root",
     "password": process.env.DATABASE_PASS || "root",
