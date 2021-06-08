2e4bac62c1533727772050f5c7ca36b8efc22039
Goytai <dev.goytai@gmail.com>
2021-06-08 15:08:39 -0300

    Fix: ssl problem

--- ormconfig.js
+++ ormconfig.js
@@ -1,4 +1,4 @@
-const ssl = false
+const ssl = process.env.NODE_ENV === 'production ' ? {"rejectUnauthorized": false} : false
 const entities = process.env.NODE_ENV === 'production ' ? "./dist/**/entities/*.js" : "./src/**/entities/*.ts"
 const migrations = process.env.NODE_ENV === 'production ' ? "./dist/shared/typeorm/migrations/*.js" :"./src/shared/typeorm/migrations/*.ts"
 const migrationsDir = process.env.NODE_ENV === 'production ' ? "./dist/shared/typeorm/migrations" : "./src/shared/typeorm/migrations"
